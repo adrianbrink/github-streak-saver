ca614292bd526c34cbfb4f67eb17cb4ed8735b92
Adrian Brink <adrian@brink-holdings.com>
2016-12-19 12:39:25 +0100

    Update to work with the current github  api

--- send_sms.py
+++ send_sms.py
@@ -4,9 +4,19 @@ import json
 import sys
 
 import requests
+from HTMLParser import HTMLParser
 from twilio.rest import TwilioRestClient
 
 import conf
+
+class CustomHTMLParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.rects = []
+
+    def handle_starttag(self, tag, attrs):
+        if tag == "rect":
+            self.rects.append(attrs)
 
 
 def send(s):
@@ -20,16 +30,21 @@ message = sys.argv[1] if len(sys.argv) > 1 else default_message
 client = TwilioRestClient(conf.ACCOUNT_SID, conf.AUTH_TOKEN)
 
 # Get Github contributions activity
-url = 'https://github.com/users/%s/contributions_calendar_data' % conf.USERNAME
+#url = 'https://github.com/users/%s/contributions_calendar_data' % conf.USERNAME
+url = 'https://github.com/users/%s/contributions' % conf.USERNAME
 request = requests.get(url)
 if request.ok:
     try:
-        data = json.loads(request.text)
+        parser = CustomHTMLParser()
+        parser.feed(request.text)
+
+        # Get the last column that signify today's commits
+        d = dict(parser.rects[-1])
 
         # Get the number of commits made today
-        commits_today = data[-1][1]
+        comits_today = d["data-count"]
 
-        if not commits_today:
+        if comits_today==0:
             send(message)
     except:
         send('There was an error getting the number of commits today')
